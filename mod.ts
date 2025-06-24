9e801bc251ddb0e34f920123614ea02813dc0117
ss2552 <85072753+ss2552@users.noreply.github.com>
2025-06-24 18:46:07 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,22 +1,38 @@
-import "https://deno.land/std@0.224.0/dotenv/load.ts"
+import "https://deno.land/std@0.224.0/dotenv/load.ts";
 
 // https://docs.deno.com/api/web/~/WebSocket
-const ws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json")
+const ws = new WebSocket("wss://gateway.discord.gg/?v=10&encoding=json");
 
-const DISCORD_BOT_TOKEN = Deno.env.get("TOKEN") | Error("トークンが無いわ");
+const DISCORD_BOT_TOKEN = Deno.env.get("TOKEN") || Error("トークンが無いわ");
 
-ws.onopen=_=>ws.send(JSON.stringify({op: 2, d: {token: DISCORD_BOT_TOKEN, compress: true}}))
+console.log(DISCORD_BOT_TOKEN)
 
-ws.onmessage = ({data}) => {
-  
-  const {op, d} = JSON.parse(data)
-  
-  switch (op){
-    case 10 : setInterval(_=>ws.send("{\"op\":1, \"d\":0}")), d.heartbeat_interval)
-    case  0 : return
-    default : console.log(op)
-  }
+ws.onopen=_=>ws.send(JSON.stringify({
+        "op": 2,
+        "d":{
+            "token": DISCORD_BOT_TOKEN,
+            "intents": 4,
+            "properties":{
+                "os": Deno.build.os
+                }
+            }
+        }))
+ws.onmessage = ({ data }) => {
+    const {op, d} = JSON.parse(data);
+
+    switch (op) {
+        case 10:
+            setInterval(_=> ws.send(JSON.stringify({"op":1, "d":null})), d.heartbeat_interval)
+            break
+
+        case 0:
+            break
+            
+        default:
+            console.log(op);
+    }
+};
 
 ws.onclose = (event) => {
-  console.log(`WebSocket closed: Code=${event.code}, Reason=${event.reason}`);
+    console.log(`WebSocket closed: Code=${event.code}, Reason=${event.reason}`);
 }
